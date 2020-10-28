06f63cd412c16aa0d484fe197d1295e01823f1be
njpontes1 <69497131+njpontes1@users.noreply.github.com>
2020-10-28 05:16:51 +0000

    .
    .

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1603862195,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/l-sheet0.png",
+		"images/r-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/wip-sheet0.png",
+		"images/wip-sheet1.png",
+		"images/wip-sheet2.png",
+		"media/music.m4a",
+		"media/[ontiva.com] offical uk playstation magazine demo disk music_ menu-hq.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
